1149a608feb6d3a123169c468f16d8f4601de6b1
Tyler Degand <tylerdegand@gmail.com>
2020-09-03 12:33:01 -0500

    almost done with routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const Sequelize = require("sequelize")
-const { models } = require('./models');
+const { User, Course } = require('./models');
 const { body } = require('express-validator');
 
 // variable to enable global error logging
@@ -31,42 +31,118 @@ sequelize.authenticate()
     console.error('Unable to connect to the database:', err);
   });
 
-// Get the currently Authenticated user
-app.get('/api/users', (req, res) => {
+  function asyncHandler(cb){
+    return async(req, res, next) => {
+      try {
+        await cb(req, res, next)
+      } catch(error){
+        res.status(500).send(error);
+      }
+    }
+  }
 
-});
+// Get the currently Authenticated user
+app.get('/api/users', asyncHandler(async(req, res) => {
+  try {
+     await User.findByPk(req.params.id).then(user => {
+      res.status(200)
+      res.json({ user })
+  })
+  } catch(error) {
+      res.status(404)
+      res.json({ error })
+  }
+}));
 
 // Creates a user, sets the Location header to "/", and returns no content
-app.post('/api/users', (req, res) => {
-
-})
+app.post('/api/users', asyncHandler(async(req, res) => {
+    try{
+      res.status(201)
+      await User.create({  firstname: req.body.firstName, lastName: req.body.lastName, emailAddress: req.body.emailAddress, password: req.body.password })
+    }catch(error) {
+      res.status(400)
+      res.json({ error });
+      
+    }
+}));
 
 // Returns a list of courses (including the user that owns each course)
-app.get('/api/courses', (req, res) => {
-
-});
+app.get('/api/courses', asyncHandler(async(req, res) => {
+  try {
+     await Course.findAll().then(course => {
+       res.status(200)
+       res.json({ course })
+     })
+  } catch(error) {
+      res.status(404)
+      res.json({ error })
+  }
+}));
 
 // Returns the course (including the user that owns the course) for the provided course ID
-app.get('/api/courses/:id', [
-    body()
-], (req, res) => {
+app.get('/api/courses/:id', asyncHandler(async(req, res) => {
+  try {
+     await Course.findByPk(req.params.id).then(course => {
+      res.status(200)
+      res.json({ course })
+  })
+  } catch(error) {
+      res.status(404)
+      res.json( {
+        message: "Sorry something went wrong"
+      },
+      { error })
+    }
+  }))
 
-});
 
 // Creates a course, sets the Location header to the URI for the course, and returns no content
-app.post('/api/courses', (req, res) => {
-
-});
+app.post('/api/courses', asyncHandler(async(req, res) => {
+  try{
+    res.status(201)
+    await Course.create({  title: req.body.title, description: req.body.description, estimatedTime: req.body.estimatedTime, materialsNeeded: req.body.materialsNeeded })
+  }catch(error) {
+    res.status(400)
+    res.json( {
+      message: "Sorry something went wrong"
+    },
+    { error })
+  }
+}));
 
 // Updates a course and returns no content
-app.put('/api/courses/:id', (req, res) => {
-
-});
+app.put('/api/courses/:id', asyncHandler(async(req, res) => {
+  try{
+    res.status(204)
+    await Course.update({  title: req.body.title, description: req.body.description, estimatedTime: req.body.estimatedTime, materialsNeeded: req.body.materialsNeeded }, {
+      where: {
+        id: req.params.id
+      }
+    })
+  }catch(error) {
+    res.status(400)
+    res.json({ error });
+  }
+}));
 
 // deletes a course and returns no content
-app.delete('/api/courses/:id', (req, res) => {
-
-});
+app.delete('/api/courses/:id', asyncHandler(async(req, res) => {
+  try{
+    await Course.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(204)
+  } catch(error) {
+    res.status(400)
+    res.json( {
+      message: "Sorry something went wrong"
+    },
+    { error })
+  }
+  
+}));
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
